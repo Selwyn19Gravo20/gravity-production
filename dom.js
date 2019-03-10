4fb8b895db3ef26f2852af827c505e9aa69ac184
Selwyn19Gravo20 <48410953+Selwyn19Gravo20@users.noreply.github.com>
2019-03-10 11:30:33 -0500

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,12 @@
+
+<div>
+
+<button onclick="Alert('This page works best in the latest version of Google Chrome')" class="btn btn-dark btn block"
+id="button">Message Alert!</button>
+
+
+</div>
+
+<script src="dom.js"></script>
+</body>
+</html>
